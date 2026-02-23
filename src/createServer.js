10d11e906518e232f4fa1f6fc84ce50d7169e1f9
4xmplme <andrii_kondratenko@icloud.com>
2026-02-23 22:51:25 +0200

    fix task solution according to ai mentor's suggestion

--- src/createServer.js
+++ src/createServer.js
@@ -77,9 +77,27 @@ function createServer() {
             return;
           }
 
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify(expense));
+          if (req.headers['content-type'] === 'application/json') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(expense));
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+
+            res.end(`
+              <!DOCTYPE html>
+              <html lang="en">
+              <head>
+                <meta charset="UTF-8">
+                <title>Expense Saved</title>
+              </head>
+              <body>
+                <pre>${JSON.stringify(expense, null, 2)}</pre>
+              </body>
+              </html>
+            `);
+          }
         });
       });
 
